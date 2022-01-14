91b3a78fe34797c74c91ea1f1673877b9d716845
GabiFerraz <ferrazmgabi@gmail.com>
2022-01-14 13:05:54 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const MINCHARACTERS = 2;
+
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      valueName: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  activateButton = ({ target: { value } }) => {
+    this.setState({
+      valueName: value,
+      isButtonDisabled: value.length < MINCHARACTERS,
+    });
+  }
+
   render() {
+    const { isButtonDisabled, valueName } = this.state;
     return (
-      <>
-        <div data-testid="page-search" />
+      <section data-testid="page-search">
         <Header />
-      </>
+        <form>
+          <label htmlFor="bandArtist">
+            <input
+              type="text"
+              id="bandArtist"
+              data-testid="search-artist-input"
+              onChange={ this.activateButton }
+              value={ valueName }
+              placeholder="Nome do Artista"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisabled }
+            onClick={ this.activateButton }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </section>
     );
   }
 }
